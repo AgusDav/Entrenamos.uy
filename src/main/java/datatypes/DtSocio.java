ea1e42c423f659b9d121fa4f57cbb8e5127f22fc
JoacoBalsa <joaquin.balsa2002@gmail.com>
2023-08-15 11:37:31 -0300

    Todos los datatypes y sus creadores/getters/setters

--- src/main/java/datatypes/DtSocio.java
+++ src/main/java/datatypes/DtSocio.java
@@ -1,5 +1,9 @@
 package datatypes;
 
-public class DtSocio {
+public class DtSocio extends DtUsuario{
+
+	public DtSocio(String n, String nom, String a, String e, DtFecha f) {
+		super(n, nom, a, e, f);
+	}
 
 }
